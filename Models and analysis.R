1e25ec7e48c3f4d9efb2d4e07016dbe09b1f87a1
kimlong0 <kimlong0@uw.edu>
2022-11-26 23:59:32 -0800

    adding starter plots

--- Models and analysis.R
+++ Models and analysis.R
@@ -1,1 +1,15 @@
+library(ggplot2)
+
+install.packages('dplyr')
+library(dplyr)
+
 loan.df <- read.csv('Loan_Data.csv', stringsAsFactors = TRUE)
+
+str(loan.df)
+
+
+ggplot(data = loan.df) + geom_point(aes(x = ApplicantIncome, y = LoanAmount, color = Loan_Status))
+
+ggplot(data = loan.df) + geom_point(aes(x = Loan_Amount_Term, y = LoanAmount))
+
+ggplot(data = loan.df) + geom_bar(aes(x = Property_Area))
